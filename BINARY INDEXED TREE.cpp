4fe33c766c6c946e0cb93d6b11a6fff035924c05
Alextz307 <67068614+Alextz307@users.noreply.github.com>
2021-10-18 12:42:38 +0300

    Update BINARY INDEXED TREE.cpp

--- BINARY INDEXED TREE.cpp
+++ BINARY INDEXED TREE.cpp
@@ -2,14 +2,16 @@ const int MAXN = 1e5;
 int n, aib[1 + MAXN];
 
 void update(int x, int t) {
-  for (int i = x; i <= n; i += i & -i)
+  for (int i = x; i <= n; i += i & -i) {
     aib[i] += t;
+  }
 }
 
 int query(int x) {
   int ans = 0;
-  for (int i = x; i > 0; i -= i & -i)
+  for (int i = x; i >= 1; i -= i & -i) {
     ans += aib[i];
+  }
   return ans;
 }
 
