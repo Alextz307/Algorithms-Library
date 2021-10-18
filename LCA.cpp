c7dbe8419ebfd165df2844eef14739e937c25838
Alextz307 <67068614+Alextz307@users.noreply.github.com>
2021-10-18 12:43:29 +0300

    Update LCA.cpp

--- LCA.cpp
+++ LCA.cpp
@@ -15,28 +15,34 @@ void dfs(int u, int level) {
 }
 
 void compute_rmq() {
-  for (int i = 2; i <= m; ++i)
+  for (int i = 2; i <= m; ++i) {
     lg2[i] = lg2[i >> 1] + 1;
-  for (int i = 1; i <= m; ++i)
+  }
+  for (int i = 1; i <= m; ++i) {
     rmq[0][i] = i;
-  for (int i = 1; (1 << i) < m; ++i)
+  }
+  for (int i = 1; (1 << i) < m; ++i) {
     for (int j = 1; j <= m - (1 << i); ++j) {
       int l = 1 << (i - 1);
       rmq[i][j] = rmq[i - 1][j];
-      if (depth[rmq[i - 1][j + l]] < depth[rmq[i][j]])
+      if (depth[rmq[i - 1][j + l]] < depth[rmq[i][j]]) {
         rmq[i][j] = rmq[i - 1][j + l];
+      }
     }
+  }
 }
 
 int find_lca(int u, int v) {
   int x = first[u], y = first[v];
-  if (x > y)
+  if (y < x) {
     swap(x, y);
+  }
   int diff = y - x + 1;
   int l = lg2[diff];
   int sol = rmq[l][x];
   int shift = diff - (1 << l);
-  if (depth[sol] > depth[rmq[l][x + shift]])
+  if (depth[sol] > depth[rmq[l][x + shift]]) {
     sol = rmq[l][x + shift];
+  }
   return tour[sol];
 }
