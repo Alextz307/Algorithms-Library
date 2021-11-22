2c28ccfa93e31263a92785be8bb0e2b3ae5b1fd3
Alextz307 <67068614+Alextz307@users.noreply.github.com>
2021-11-22 20:56:04 +0200

    Update CHT.cpp

--- CHT.cpp
+++ CHT.cpp
@@ -1,42 +1,58 @@
-/// MAX-HULL
+/// MIN-HULL
 const int kN = 1e5;
 const int INF = -2e9;
-
+ 
+struct func {
+  int m, b, fIndex;
+ 
+  void read() {
+    fin >> m >> b;
+  }
+ 
+  bool operator < (const func &A) const {
+    return m > A.m;
+  }
+} f[kN];
+ 
 struct line {
   ld start;
-  int m, b;
+  int m, b, fIndex;
  
-  int eval(int x) {
-    return m * x + b;
+  int64_t eval(int x) {
+    return (int64_t)m * x + b;
   }
 };
  
 struct CHT {
-  line hull[kN];
+  vector<line> hull;
   int l = 0, r = -1;
+ 
+  CHT(int n) {
+    hull.resize(n);
+  }
  
   ld findIntersection(int m1, int b1, int m2, int b2) {
     return (ld)(b2 - b1) / (m1 - m2);
   }
  
-  void addLine(int m, int b) {
+  void addLine(int m, int b, int fIndex) {
     while (l <= r) {
       line l = hull[r];
-      if (l.m * l.start + l.b > m * l.start + b) {
+      if ((int64_t)l.m * l.start + l.b < (int64_t)m * l.start + b) {
         if (l.m != m) {
-          hull[++r] = {findIntersection(l.m, l.b, m, b), m, b};
+          hull[++r] = {findIntersection(l.m, l.b, m, b), m, b, fIndex};
         }
         return;
       }
       --r;
     }
-    hull[++r] = {INF, m, b};
+    hull[++r] = {INF, m, b, fIndex};
   }
  
   int query(int x) {
-    while (l + 1 <= r && hull[l].eval(x) <= hull[l + 1].eval(x)) {
-      ++l;
-    }
-    return hull[l].eval(x);
+    line ln = *--upper_bound(hull.begin() + l, hull.begin() + r + 1, x, [&](int y, line ll) -> bool {
+      return y < ll.start;
+    });
+    return ln.fIndex;
   }
 };
