f3c5b2c20d01d965345c5bb3f77640684cd7a8b9
Alextz307 <67068614+Alextz307@users.noreply.github.com>
2022-03-31 15:24:06 +0300

    Update DINIC.cpp

--- DINIC.cpp
+++ DINIC.cpp
@@ -1,74 +1,116 @@
-struct flow_edge {
-  int u, v, cap;
+#include <bits/stdc++.h>
+#define INF 0x3f3f3f3f
 
-  flow_edge(int _u, int _v, int _cap) : u(_u), v(_v), cap(_cap) {}
+using namespace std;
+
+ifstream fin("maxflow.in");
+ofstream fout("maxflow.out");
+
+struct edge {
+  int u, v, c;
+
+  edge() {}
+
+  edge(int _u, int _v, int _c) : u(_u), v(_v), c(_c) {}
 };
 
 struct Dinic {
   int n, m = 0, s, t;
-  vector<flow_edge> edges;
-  vector<vector<int>> adj;
-  vector<int> level, ptr;
-  queue<int> q;
+  vector<edge> edges;
+  vector<vector<int>> g;
+  vector<int> q, level, ptr;
 
-  Dinic(int N, int source, int sink) : n(N), s(source), t(sink) {
-    adj.resize(n + 1);
+  Dinic(int _n, int _m, int _s, int _t) : n(_n), s(_s), t(_t) {
+    edges.resize(2 * _m);
+    g.resize(n + 1);
+    q.resize(n + 1);
     level.resize(n + 1);
     ptr.resize(n + 1);
   }
 
-  void add_edge(int u, int v, int cap) {
-    edges.emplace_back(u, v, cap);
-    adj[u].emplace_back(m++);
-    edges.emplace_back(v, u, 0);
-    adj[v].emplace_back(m++);
+  void addEdge(int u, int v, int c) {
+    g[u].emplace_back(m);
+    edges[m++] = {u, v, c};
+    g[v].emplace_back(m);
+    edges[m++] = {v, u, 0};
   }
 
   bool bfs() {
-    while (!q.empty()) {
-      int curr = q.front();
-      q.pop();
-      for (int id : adj[curr])
-        if (level[edges[id].v] == -1 && edges[id].cap) {
-          level[edges[id].v] = level[curr] + 1;
-          q.emplace(edges[id].v);
+    for (int i = 1; i <= n; ++i) {
+      level[i] = -1;
+    }
+    level[s] = 0;
+    int l = 0, r = -1;
+    q[++r] = s;
+    while (l <= r) {
+      int u = q[l++];
+      for (int id : g[u]) {
+        if (level[edges[id].v] == -1 && edges[id].c) {
+          level[edges[id].v] = level[u] + 1;
+          q[++r] = edges[id].v;
         }
+      }
     }
     return level[t] != -1;
   }
 
-  int dfs(int u, int curr_flow) {
-    if (curr_flow == 0)
+  int dfs(int u, int flow) {
+    if (flow == 0) {
       return 0;
-    if (u == t)
-      return curr_flow;
-    for (int& p = ptr[u]; p < static_cast<int>(adj[u].size()); ++p) {
-      int id = adj[u][p];
+    }
+    if (u == t) {
+      return flow;
+    }
+    for (int &p = ptr[u]; p < (int)g[u].size(); ++p) {
+      int id = g[u][p];
       int v = edges[id].v;
-      if (level[u] + 1 != level[v] || edges[id].cap <= 0)
+      int c = edges[id].c;
+      if (level[v] != level[u] + 1 || c == 0) {
         continue;
-      int new_flow = dfs(v, min(curr_flow, edges[id].cap));
-      if (new_flow == 0)
+      }
+      int minFlow = dfs(v, min(flow, c));
+      if (minFlow == 0) {
         continue;
-      edges[id].cap -= new_flow;
-      edges[id ^ 1].cap += new_flow;
-      return new_flow;
+      }
+      edges[id].c -= minFlow;
+      edges[id ^ 1].c += minFlow;
+      return minFlow;
     }
     return 0;
   }
 
-  int max_flow() {
+  int maxFlow() {
     int flow = 0;
-    while (true) {
-      fill(level.begin(), level.end(), -1);
-      level[s] = 0;
-      q.emplace(s);
-      if (!bfs())
-        break;
-      fill(ptr.begin(), ptr.end(), 0);
-      while (int curr_flow = dfs(s, INF))
-        flow += curr_flow;
+    while (bfs()) {
+      for (int i = 1; i <= n; ++i) {
+        ptr[i] = 0;
+      }
+      while (int newFlow = dfs(s, INF)) {
+        flow += newFlow;
+      }
     }
     return flow;
   }
 };
+
+void testCase() {
+  int n, m;
+  fin >> n >> m;
+  Dinic g(n, m, 1, n);
+  for (int i = 0; i < m; ++i) {
+    int u, v, w;
+    fin >> u >> v >> w;
+    g.addEdge(u, v, w);
+  }
+  fout << g.maxFlow() << '\n';
+}
+
+int main() {
+  int tests = 1;
+  for (int tc = 0; tc < tests; ++tc) {
+    testCase();
+  }
+  fin.close();
+  fout.close();
+  return 0;
+}
