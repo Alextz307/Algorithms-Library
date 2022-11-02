2ae31a77609d48c20e2273b68a2b1d4641b2139b
Alextz307 <67068614+Alextz307@users.noreply.github.com>
2022-11-02 19:20:35 +0200

    Update PREFFIX FUNCTION.cpp

--- PREFFIX FUNCTION.cpp
+++ PREFFIX FUNCTION.cpp
@@ -1,12 +1,14 @@
-vector<int> preffix_function(string &s) { /// cel mai lung prefix care este si sufix(sirul complet este exclus)
+vector<int> preffix_function(string s) { /// cel mai lung prefix care este si sufix(sirul complet este exclus)
   int n = s.length();
   vector<int> pi(n + 1);
   s = '#' + s;
-  for (int i = 2, q = 0; i <= N; ++i) {
-    while (q && s[q + 1] != s[i])
+  for (int i = 2, q = 0; i <= n; ++i) {
+    while (q && s[q + 1] != s[i]) {
       q = pi[q];
-    if (s[q + 1] == s[i])
+    }
+    if (s[q + 1] == s[i]) {
       ++q;
+    }
     pi[i] = q;
   }
   return pi;
