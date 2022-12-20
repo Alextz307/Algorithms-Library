2579714bdcc82a27d4ce292836dba2ff24011dda
Alextz307 <67068614+Alextz307@users.noreply.github.com>
2022-12-20 21:34:32 +0200

    Update and rename Z FUNCTION.cpp to Z Function.cpp

--- Z Function.cpp
+++ Z Function.cpp
@@ -1,4 +1,4 @@
-vector<int> z_function(const string &s) { /// z[i] - cea mai lunga subsecventa ce incepe pe pozitia i si este prefix al intregului sir
+vector<int> zFunction(const string &s) { /// z[i] - cea mai lunga subsecventa ce incepe pe pozitia i si este prefix al intregului sir
   int n = s.length();
   vector<int> z(n);
   for (int i = 0, l = 0, r = 0; i < n; ++i) {
