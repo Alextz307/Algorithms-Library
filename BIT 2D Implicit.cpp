929563a4f9b5752d2d2f4dade6c1ce70b526df87
Alexandru Lorintz <67068614+Alextz307@users.noreply.github.com>
2023-07-13 18:21:07 +0300

    Update BIT 2D Implicit.cpp

--- BIT 2D Implicit.cpp
+++ BIT 2D Implicit.cpp
@@ -8,9 +8,9 @@ struct FenwickTree2D {
     aib.resize(n + 1);
   }
 
-  void allocate(int x, int v) {
+  void allocate(int x, int y) {
     for (int i = x; i <= n; i += i & -i) {
-      vals[i].emplace_back(v);
+      vals[i].emplace_back(y);
     }
   }
 
@@ -18,25 +18,25 @@ struct FenwickTree2D {
     for (int i = 1; i <= n; ++i) {
       sort(vals[i].begin(), vals[i].end());
       vals[i].erase(unique(vals[i].begin(), vals[i].end()), vals[i].end());
-      aib[i].resize(1 + vals[i].size());
+      aib[i].resize(vals[i].size() + 1);
     }
   }
 
-  void updateAib(int x, int v, int f) {
-    int pos = upper_bound(vals[x].begin(), vals[x].end(), v) - vals[x].begin();
+  void updateAib(int x, int y, int v) {
+    int pos = upper_bound(vals[x].begin(), vals[x].end(), y) - vals[x].begin();
     for (int i = pos; i < (int)aib[x].size(); i += i & -i) {
-      aib[x][i] += f;
+      aib[x][i] += v;
     }
   }
 
-  void update(int x, int v, int f) {
+  void update(int x, int y, int v) {
     for (int i = x; i <= n; i += i & -i) {
-      updateAib(i, v, f);
+      updateAib(i, y, v);
     }
   }
 
-  int queryAib(int x, int v) {
-    int pos = upper_bound(vals[x].begin(), vals[x].end(), v) - vals[x].begin();
+  int queryAib(int x, int y) {
+    int pos = upper_bound(vals[x].begin(), vals[x].end(), y) - vals[x].begin();
     int ans = 0;
     for (int i = pos; i > 0; i = i & (i - 1)) {
       ans += aib[x][i];
@@ -51,10 +51,10 @@ struct FenwickTree2D {
     return queryAib(x, r) - queryAib(x, l - 1);
   }
 
-  int query(int x, int v) {
+  int query(int x, int y) {
     int ans = 0;
     for (int i = x; i > 0; i = i & (i - 1)) {
-      ans += queryAib(i, v);
+      ans += queryAib(i, y);
     }
     return ans;
   }
