59f389fcf5b9959a04488fe684cef27b70fb3644
Alexandru Lorintz <67068614+Alextz307@users.noreply.github.com>
2023-08-20 23:15:42 +0300

    Update Kosaraju's algorithm for finding strongly connected components.cpp

--- Kosaraju's algorithm for finding strongly connected components.cpp
+++ Kosaraju's algorithm for finding strongly connected components.cpp
@@ -1,70 +1,91 @@
 #include <bits/stdc++.h>
-#define nmax 100005
+#define nmax 100001
 
 using namespace std;
 
-ifstream f ("ctc.in");
-ofstream g ("ctc.out");
+ifstream f("ctc.in");
+ofstream g("ctc.out");
 
-int n, m, x, y, viz[nmax], nr;
-vector < int > G[nmax], GT[nmax], CTC[nmax];
-stack < int > S;
+int n, m, nr, viz[nmax];
+vector<int> G[nmax], GT[nmax], CTC[nmax];
+stack<int> S;
 
-void read () {
-    f >> n >> m;
-    while (m --) {
-        f >> x >> y;
-        G[x].push_back (y);
-        GT[y].push_back (x);
+void read() {
+  f >> n >> m;
+  
+  for (int i = 0; i < m; ++i) {
+    int x, y;
+    f >> x >> y;
+    
+    G[x].push_back(y);
+    GT[y].push_back(x);
+  }
+}
+
+void DFSP(int k) {
+  viz[k] = 1;
+  
+  for (unsigned int i = 0; i < G[k].size(); ++i) {
+    int vecin = G[k][i];
+    
+    if (!viz[vecin]) {
+      DFSP(vecin);
     }
+  }
+  
+  S.push(k);
 }
 
-void DFSP (int k) {
-    viz[k] = 1;
-    for (unsigned int i = 0; i < G[k].size(); ++i) {
-        int vecin = G[k][i];
-        if (!viz[vecin]) DFSP (vecin);
+void DFSM(int k) {
+  viz[k] = 2;
+  CTC[nr].push_back(k);
+  
+  for (unsigned int i = 0; i < GT[k].size(); ++i) {
+    int vecin = GT[k][i];
+    
+    if (viz[vecin] == 1) {
+      DFSM(vecin);
     }
-    S.push (k);
+  }
 }
 
-void DFSM (int k) {
-    viz[k] = 2;
-    CTC[nr].push_back (k);
-    for (unsigned int i = 0; i < GT[k].size(); ++i) {
-        int vecin = GT[k][i];
-        if (viz[vecin] == 1) DFSM (vecin);
+void solve() {
+  for (int i = 1; i <= n; ++i) {
+    if (!viz[i]) {
+      DFSP(i);
     }
-}
-
-void solve () {
-    for (int i = 1; i <= n; ++i) if (!viz[i]) DFSP (i);
-    while (!S.empty()) {
-        int nod = S.top();
-        S.pop();
-        if (viz[nod] == 1) {
-            nr ++;
-            DFSM (nod);
-        }
+  }
+  
+  while (!S.empty()) {
+    int nod = S.top();
+    S.pop();
+    
+    if (viz[nod] == 1) {
+      nr += 1;
+      DFSM(nod);
     }
+  }
 }
 
-void print () {
-    g << nr << '\n';
-    for (int i = 1; i <= nr; ++i, g << '\n')
-    for (unsigned int j = 0; j < CTC[i].size(); ++j)
-    g << CTC[i][j] << " ";
+void print() {
+  g << nr << '\n';
+  
+  for (int i = 1; i <= nr; ++i, g << '\n') {
+    for (unsigned int j = 0; j < CTC[i].size(); ++j) {
+      g << CTC[i][j] << ' ';
+    }
+  }
 }
 
-void Close () {
-    f.close();
-    g.close();
+void Close() {
+  f.close();
+  g.close();
 }
 
-int main()
-{
-    read();
-    solve();
-    print();
-    return 0;
+int main() {
+  read();
+  solve();
+  print();
+  Close();
+  return 0;
 }
