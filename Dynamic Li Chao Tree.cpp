35427c1c3fb9b0cddf02ca166e51d6f03903f242
Alexandru Lorintz <67068614+Alextz307@users.noreply.github.com>
2023-09-20 10:05:22 +0300

    Update Dynamic Li Chao Tree.cpp

--- Dynamic Li Chao Tree.cpp
+++ Dynamic Li Chao Tree.cpp
@@ -30,11 +30,14 @@ void addLine(node* x, int lx, int rx, line l) {
     return;
   }
   int mid = (lx + rx) >> 1;
+  
   if (l.m < x->l.m) {
     swap(x->l, l);
   }
+  
   if (l.eval(mid) < x->l.eval(mid)) {
     swap(x->l, l);
+    
     if (x->rSon) {
       addLine(x->rSon, mid + 1, rx, l);
     } else {
@@ -51,10 +54,13 @@ void addLine(node* x, int lx, int rx, line l) {
  
 int64_t query(node* x, int lx, int rx, int pos) {
   int64_t val = x->l.eval(pos);
+  
   if (lx == rx) {
     return val;
   }
+  
   int mid = (lx + rx) >> 1;
+  
   if (pos <= mid) {
     if (x->lSon) {
       minSelf(val, query(x->lSon, lx, mid, pos));
@@ -64,5 +70,6 @@ int64_t query(node* x, int lx, int rx, int pos) {
       minSelf(val, query(x->rSon, mid + 1, rx, pos));
     }
   }
+  
   return val;
 }
