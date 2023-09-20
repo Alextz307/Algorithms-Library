f0aa54a9677b1a14de962519a00ec10a52815fe6
Alexandru Lorintz <67068614+Alextz307@users.noreply.github.com>
2023-09-20 10:04:40 +0300

    Update Dynamic Convex Hull Trick.cpp

--- Dynamic Convex Hull Trick.cpp
+++ Dynamic Convex Hull Trick.cpp
@@ -10,56 +10,74 @@ struct line {
     if (rhs.b != INF) {
       return m < rhs.m;
     }
+    
     const line* s = succ();
+    
     if (!s) {
       return 0;
     }
+    
     int64_t x = rhs.m;
+    
     return b - s->b < (s->m - m) * x;
   }
 };
 
 struct dynamicHull : public multiset<line> {
   const int64_t inf = -INF;
+
   bool bad(iterator y) {
     auto z = next(y);
+    
     if (y == begin()) {
       if (z == end()) {
         return 0;
       }
+      
       return y->m == z->m && y->b <= z->b;
     }
+    
     auto x = prev(y);
+    
     if (z == end()) {
       return y->m == x->m && y->b <= x->b;
     }
+    
     int64_t v1 = (x->b - y->b);
+    
     if (y->m == x->m) {
       v1 = x->b > y->b ? inf : -inf;
     } else {
       v1 /= (y->m - x->m);
     }
+    
     int64_t v2 = (y->b - z->b);
+    
     if (z->m == y->m) {
       v2 = y->b > z->b ? inf : -inf;
     } else {
       v2 /= (z->m - y->m);
     }
+    
     return v1 >= v2;
   }
 
   void insertLine(int64_t m, int64_t b) {
     auto y = insert({m, b});
+    
     y->succ = [=] {
       return next(y) == end() ? 0 : &*next(y);
     };
+    
     if (bad(y)) {
       erase(y);
       return;
     }
+    
     while (next(y) != end() && bad(next(y))) {
       erase(next(y));
     }
+    
     while (y != begin() && bad(prev(y))) {
       erase(prev(y));
     }
@@ -67,6 +85,7 @@ struct dynamicHull : public multiset<line> {
 
   int64_t eval(int64_t x) {
     auto l = *lower_bound((line) {x, INF});
+    
     return l.m * x + l.b;
   }
 };
