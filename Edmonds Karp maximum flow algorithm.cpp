f9654a60b48de44935a3a3862bc365291f9200b1
Alexandru Lorintz <67068614+Alextz307@users.noreply.github.com>
2023-09-20 10:06:57 +0300

    Update Edmonds Karp maximum flow algorithm.cpp

--- Edmonds Karp maximum flow algorithm.cpp
+++ Edmonds Karp maximum flow algorithm.cpp
@@ -20,40 +20,51 @@ bool bfs() {
   for (int i = 1; i <= n; ++i) {
     par[i] = 0;
   }
+  
   int l = 0, r = -1;
   par[s] = -1;
   q[++r] = s;
+  
   while (l <= r) {
     int u = q[l++];
+    
     for (int v : g[u]) {
       if (par[v] == 0 && C[u][v]) {
         par[v] = u;
+        
         if (v != t) {
           q[++r] = v;
         }
       }
     }
   }
+  
   par[s] = 0;
+  
   return par[t] != 0;
 }
 
 int maxFlow() {
   int flow = 0;
+  
   while (bfs()) {
     for (int u : g[t]) {
       if (par[u] != 0) {
         par[t] = u;
         int minFlow = INF;
+        
         for (int v = t; v != s; v = par[v]) {
           minSelf(minFlow, C[par[v]][v]);
+          
           if (minFlow == 0) {
             break;
           }
         }
+        
         if (minFlow == 0) {
           continue;
         }
+        
         flow += minFlow;
         for (int v = t; v != s; v = par[v]) {
           C[par[v]][v] -= minFlow;
@@ -62,11 +73,13 @@ int maxFlow() {
       }
     }
   }
+  
   return flow;
 }
 
 void testCase() {
   fin >> n >> m;
+  
   for (int i = 0; i < m; ++i) {
     int u, v, w;
     fin >> u >> v >> w;
@@ -74,16 +87,20 @@ void testCase() {
     g[u].emplace_back(v);
     g[v].emplace_back(u);
   }
+  
   s = 1, t = n;
   fout << maxFlow() << '\n';
 }
 
 int main() {
   int tests = 1;
+  
   for (int tc = 0; tc < tests; ++tc) {
     testCase();
   }
+  
   fin.close();
   fout.close();
+  
   return 0;
 }
