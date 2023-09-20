0154c082f67e761f8dd5ad7087af7af4ee64b9b5
Alexandru Lorintz <67068614+Alextz307@users.noreply.github.com>
2023-09-20 10:08:10 +0300

    Delete Random Seed.cpp

--- Random Seed.cpp
+++
@@ -1,9 +0,0 @@
-uint64_t randomAddress() {
-  char *p = new char;
-  delete p;
-  return uint64_t(p);
-}
- 
-const uint64_t SEED = chrono::steady_clock::now().time_since_epoch().count() * (randomAddress() | 1);
-mt19937_64 rng(SEED);
- 
