db96586d0e594e9ea8ad8da2ea8c019ddd3f019b
Alexandru Lorintz <67068614+Alextz307@users.noreply.github.com>
2023-09-20 10:03:16 +0300

    Update Disjoint Sets Union with rollback.cpp

--- Disjoint Sets Union with rollback.cpp
+++ Disjoint Sets Union with rollback.cpp
@@ -14,30 +14,37 @@ struct DSU {
     if (x == p[x]) {
       return x;
     }
+    
     return root(p[x]);
   }
 
   void unite(int u, int v) {
     int x = root(u), y = root(v);
+    
     if (x == y) {
       return;
     }
+    
     if (sz[x] > sz[y]) {
       swap(x, y);
     }
+    
     p[x] = y;
     sz[y] += sz[x];
     st.emplace(x);
-    --cnt;
+    
+    cnt -= 1;
   }
 
   void rollback(int checkpoint) {
     while ((int)st.size() > checkpoint) {
       int x = st.top();
       st.pop();
+      
       sz[p[x]] -= sz[x];
       p[x] = x;
-      ++cnt;
+      
+      cnt += 1;
     }
   }
 };
